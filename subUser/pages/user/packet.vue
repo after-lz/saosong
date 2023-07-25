a4ba6bb3c0609bc2ba64348eb4ab7bf69530135f
lz <942233477@qq.com>
2023-07-25 09:33:48 +0800

    红包相关功能

--- subUser/pages/user/packet.vue
+++ subUser/pages/user/packet.vue
@@ -1,48 +1,47 @@
 <template>
 	<view class="gt_content">
-		<view class="info">
-			<view class="info_left">
-				<view class="balance">100.00</view>
+		<view class="info" :style="{'background-image': `url(${gtCommon.getOssImg('user/card_bg.png')})`}">
+			<view class="info_top">
+				<view>下次清零时间：{{ gtCommon.formateTime(data.nextClearTime, 'YYYY-MM-DD') }}</view>
+				<view @click="goRecord">红包记录</view>
+			</view>
+			<view class="info_bottom">
+				<view class="balance">{{ data.money02 }}</view>
 				<view class="balanceMsg">红包余额(元)</view>
-				<view class="con_btnItem" @click="goBalance">
+				<view class="con_btnItem" @click="goBalance" :class="data.zhuanyueeSwitch ? '' : 'dis'">
 					<text>转到余额</text>
 				</view>
 			</view>
-			<view class="info_right" @click="goRecord">红包记录</view>
 		</view>
 		<view class="card">
 			<view class="card_title">做任务赢红包</view>
 			<view class="card_content">
 				<view class="card_item" v-for="item in list" :key='item.id'>
 					<template v-if="item.type === 1">
-						<text>完成企业入驻信息，得</text>
-						<text class="color_red">{{item.money}}元</text>
-						<text>红包</text>
-						<view class="btn bg_red">领取</view>
+						<view class="item_msg">
+							<image :src="gtCommon.getOssImg('user/task01.png')"></image>
+							完成企业入驻信息，得红包
+						</view>
+						<view class="btn bg_red" @click="goNext(1)">领取</view>
 					</template>
 					<template v-if="item.type === 2">
-						<text>发布专线，得</text>
-						<text class="color_red">{{item.money}}元</text>
-						<text>红包</text>
-						<view class="btn">去发布</view>
+						<view class="item_msg">
+							<image :src="gtCommon.getOssImg('user/task02.png')"></image>
+							发布专线，得红包
+						</view>
+						<view class="btn" @click="goNext(2)">去发布</view>
 					</template>
 					<template v-if="item.type === 3">
-						<text>接单赢红包，单单有</text>
-						<text class="color_red">{{item.money}}元</text>
-						<text>红包</text>
-						<view class="btn">去接单</view>
+						<view class="item_msg">
+							<image :src="gtCommon.getOssImg('user/task03.png')"></image>
+							接单赢红包，单单有红包
+						</view>
+						<view class="btn" @click="goNext(3)">去接单</view>
 					</template>
 				</view>
 			</view>
 		</view>
-		<view class="msg">
-			<text>使用须知：</text><br/>
-			<text>1、可用于专线推广费用抵扣</text><br/>
-			<text>2、红包可转到余额进行提现，进行转余额操作时需要满足转余额条件。</text><br/>
-			<text>3、红包会定时进行清空，请及时使用以免逾期清零。</text><br/>
-			<text>4、红包可以通过一下方式进行获取：完结订单得红包、分享得红包、邀请新人得红包以及新人注册红包</text><br/>
-			<text>详细规则请阅读<text class="more" @click="goRules">《红包使用规则》</text></text>
-		</view>
+		<view class="msg" v-html="data.ruleStr"></view>
 	</view>
 </template>
 
@@ -66,14 +65,61 @@
 						type: 3,
 						money: 5
 					},
-				]
+				],
+				data: {
+					nextClearTime: '', // 下次清零时间
+					money02: 0, // 红包金额
+					zhuanyueeSwitch: 0, // 是否可以转余额
+					ruleStr: '' // 规则
+				}
 			}
 		},
+		mounted() {
+			let gt = this
+			gt.getList()
+		},
 		methods: {
-			goBalance() {
-				uni.navigateTo({
-					url: './transferredBalance'
+			getList() {
+				let gt = this
+				let params = {
+					platform: 'logistics'
+				}
+				gt.gtRequest.post("/api/applogin/get_redpack_config", params).then(res=> {
+					gt.data = res
 				})
+			},
+			goBalance() {
+				// uni.navigateTo({
+				// 	url: './transferredBalance'
+				// })
+				let gt = this
+				if(!gt.data.zhuanyueeSwitch) return
+				gt.gtRequest.post("/logistics/Companywallet/apply_zhuanyuee").then(res=> {
+					uni.showToast({
+						title: res.msg
+					})
+				})
+			},
+			goNext(type) {
+				switch (type){
+					case 1:
+						uni.navigateTo({
+							url: '/subSansong/pages/sansong/companyJoin'
+						})
+						break;
+					case 2:
+						uni.navigateTo({
+							url: '/subSansong/pages/sansong/lineAdd'
+						})
+						break;
+					case 3:
+						uni.switchTab({
+							url: '/pages/index/index'
+						})
+						break;
+					default:
+						break;
+				}
 			},
 			goRecord() {
 				uni.navigateTo({
@@ -91,90 +137,105 @@
 
 <style lang="scss">
 	page {
-		background: #fff;
+		background: #F3F4F5;
+		.gt_content {
+			padding: 16rpx;
+		}
 		.info {
+			width: 100%;
+			height: 368rpx;
 			display: flex;
-			justify-content: space-between;
-			background-color: #d7d7d7;
-			.info_right {
-				text-align: right;
-				color: #169bd5;
-				padding-right: 40rpx;
+			justify-content: center;
+			align-items: center;
+			flex-wrap: wrap;
+			background-repeat: no-repeat;
+			background-size: 718rpx 368rpx;
+			margin-left: calc((100% - 718rpx)/2);
+			padding: 24rpx;
+			color: #fff;
+			.info_top {
+				width: 100%;
+				font-size: 28rpx;
+				padding-right: 0 40rpx;
+				display: flex;
+				justify-content: space-between;
 			}
-			.info_left {
-				padding: 20rpx;
+			.info_bottom {
+				display: flex;
+				justify-content: center;
+				flex-wrap: wrap;
 				.balance {
-					font-size: 42rpx;
-					font-weight: 700;
+					width: 100%;
+					text-align: center;
+					font-size: 80rpx;
 				}
 				.balanceMsg {
+					width: 100%;
 					margin: 10rpx 0;
+					text-align: center;
 				}
 				.con_btnItem {
-					width: 150rpx;
-					height: 66rpx;
-					border-radius: 10rpx;
-					color: #7f7f7f;
-					line-height: 66rpx;
+					width: 200rpx;
+					height: 72rpx;
+					border-radius: 16rpx;
+					color: #485EF4;
+					line-height: 72rpx;
 					text-align: center;
-					border: 2rpx solid #aaaaaa;
-					background-color: #f2f2f2;
+					background-color: #fff;
+					margin-top: 32rpx;
 				}
 			}
 		}
 		.card {
-			width: 94%;
-			background-color: #f3f5f7;
-			margin-top: 80rpx;
 			.card_title {
 				font-size: 34rpx;
 				font-weight: 700;
-				padding: 10rpx;
+				margin: 40rpx 0 24rpx 0;
 			}
 			.card_content {
 				display: flex;
 				flex-wrap: wrap;
-				padding-bottom: 20rpx;
+				background-color: #ffffff;
+				border-radius: 16rpx;
 				.card_item {
 					width: 100%;
-					height: 80rpx;
-					background-color: #ffffff;
-					margin: 10rpx 10rpx 0 10rpx;
+					height: 139rpx;
 					border-radius: 10rpx;
-					line-height: 80rpx;
-					position: relative;
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					padding: 0 20rpx;
+					border-bottom: 2rpx solid #f2f2f2;
+					.item_msg {
+						display: flex;
+						align-items: center;
+						image {
+							width: 80rpx;
+							height: 80rpx;
+							margin-right: 24rpx;
+						}
+					}
 					.btn {
-						width: 140rpx;
-						height: 52rpx;
-						line-height: 52rpx;
+						width: 144rpx;
+						height: 64rpx;
+						line-height: 64rpx;
 						text-align: center;
 						border-radius: 10rpx;
-						display: inline-block;
-						position: absolute;
-						right: 20rpx;
-						top: 50%;
-						transform: translate(0, -50%);
 						background-color: $gtProjectColor;
 						color: #dfebfc;
 					}
 				}
 			}
 		}
-		.color_red {
-			color: #eb5d63;
-		}
 		.bg_red {
-			background-color: #eb5d63 !important;
+			background-color: #FF6067 !important;
 		}
 		.msg {
-			padding: 20rpx;
-			>text {
-				display: inline-block;
-				margin-top: 20rpx;
-			}
-			.more {
-				color: $gtProjectColor;
-			}
+			margin-top: 40rpx;
+			color: #909399;
+		}
+		.dis {
+			color: #909399 !important;
 		}
 	}
 </style>
--- subUser/pages/user/packetRecord.vue
+++ subUser/pages/user/packetRecord.vue
@@ -1,14 +1,26 @@
 <template>
 	<view class="gt_content">
-		<view v-for="item in list" :key='item.id' class="item">
-			<view class="item_left">
-				<view class="item_title">{{ item.title }}</view>
-				<view class="item_time">{{ item.time }}</view>
-			</view>
-			<view class="item_right">
-				{{ item.money }}
-			</view>
+		<!-- <view class="time" @click="show = true">
+			{{ year }}年{{ month }}月
+			<u-icon name="arrow-down"></u-icon>
 		</view>
+		<u-picker mode="time" v-model="show" :params="params" @confirm="confirm"></u-picker> -->
+		<scroll-view class="scroll_view" scroll-y="true" @scrolltolower="loadMore">
+			<view class="con_empty" v-if="dataList.length == 0">
+				<u-empty text="暂无数据" color="#000" :src="gtCommon.getOssImg('index/empty.png')" icon-size="550"
+					margin-top="200" font-size="32"></u-empty>
+			</view>
+			<view v-for="item in dataList" :key='item.log_id' class="item">
+				<view class="item_left">
+					<view class="item_title">{{ item.remark }}</view>
+					<view class="item_time">{{ gtCommon.formateTime(item.create_time, 'YYYY-MM-DD HH:mm:SS') }}}</view>
+				</view>
+				<view class="item_right">
+					<text v-if="item.number > 0">+{{ item.number }}</text>
+					<text v-else>{{ item.number }}</text>
+				</view>
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -16,76 +28,111 @@
 	export default {
 		data() {
 			return {
-				list: [
-					{
-						id: 1,
-						title: '红包收入（发布专线奖励）',
-						time: '2022-11-11 08:08:50',
-						money: '+50.00'
-					},
-					{
-						id: 2,
-						title: '红包收入（下单奖励）',
-						time: '2022-11-11 08:08:50',
-						money: '+50.00'
-					},
-					{
-						id: 3,
-						title: '红包支出（专线推广）',
-						time: '2022-11-11 08:08:50',
-						money: '-100.00'
-					},
-				]
+				// params: {
+				// 	year: true,
+				// 	month: true,
+				// 	day: false,
+				// 	hour: false,
+				// 	minute: false,
+				// 	second: false
+				// },
+				// show: false,
+				// year: '',
+				// month: '',
+				data: {},
+				end: false,
+				dataList: []
 			}
 		},
 		mounted() {
-			let gt = this;
-			var url = '/logistics/companywallet/get_wallet_log';
-			var data= {
-				page: 1,
-				limit: 10,
-				wallet_type: 'money02',
-				data_type: '1'
-			};
-			gt.gtRequest.post(url,data).then(res=>{
-				console.log(res)
-			});
-			for (let i = 0; i < 20; i++) {
-				this.list.push({
-					id: i+1,
-					title: '红包收入（发布专线奖励）',
-					time: '2022-11-11 08:08:50',
-					money: '+50.00'
+			let gt = this
+			// let date = new Date()
+			// gt.year = date.getFullYear()
+			// gt.month = date.getMonth() + 1
+			gt.reGetDataList()
+		},
+		methods: {
+			reGetDataList() {
+				let gt = this
+				gt.dataList = []
+				gt.data = {
+					page: 1,
+					limit: 20,
+					wallet_type: 'money02',
+					data_type: 1
+				}
+				gt.end = false
+				gt.getList()
+			},
+			getList() {
+				let gt = this
+				console.log(gt.data, gt.end)
+				if (gt.end) {
+					return false
+				}
+				gt.gtRequest.post("/logistics/companywallet/get_wallet_log", gt.data).then(res=> {
+					// for (let i = 0; i < 20; i++) {
+					// 	res.list.push({
+					// 		id: i+1,
+					// 		title: '红包收入（发布专线奖励）' + (i+1),
+					// 		time: '2022-11-11 08:08:50',
+					// 		money: '+50.00'
+					// 	})
+					// }
+					gt.dataList = gt.dataList.concat(res.list)
+					if (res.list.length == gt.data.limit) {
+						gt.data.page += 1
+					} else {
+						gt.end = true
+					}
 				})
+			},
+			loadMore() {
+				console.log('loadMore')
+				let gt = this
+				gt.getList()
+			},
+			confirm(date) {
+				console.log(date.year, date.month)
 			}
 		}
 	}
 </script>
 
 <style lang='scss'>
+page {
+	background: $gtBackgroundColor;
 	.gt_content {
-		width: 94%;
-		height: 94vh;
-		margin: 20rpx auto;
-		background-color: #fff;
-		box-shadow: 10rpx 10rpx 10rpx 0rpx #ddd;
-		border-radius: 20rpx;
-		overflow: auto;
+		/deep/.u-column-item {
+			.u-text {
+				width: 0;
+			}
+		}
+		.time {
+			font-weight: 700;
+		}
+		.con_empty{
+			text-align: center;
+		}
+		.scroll_view {
+			height: 94vh;
+			overflow: auto;
+		}
 	}
 	.item {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		border-bottom: 2rpx solid #797979;
-		padding: 20rpx 40rpx 20rpx 80rpx;
-		color: #666666;
+		border-bottom: 2rpx solid #e9ebed;
+		padding: 20rpx 40rpx;
+		color: #000;
 		margin: 0 8rpx;
 		.item_left {
 			.item_title {
 				font-weight: 700;
 			}
 			.item_time {
-				color: #a2a2a2;
+				color: #a2a4a6;
 				font-size: 26rpx;
 				margin-top: 10rpx;
 			}
@@ -94,4 +141,5 @@
 			font-weight: 700;
 		}
 	}
+}
 </style>
